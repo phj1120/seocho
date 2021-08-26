1e3392a161d0f761a2fa6f95c567cca1c6cf5aa5
phj1120 <hyeonj1998@naver.com>
2021-08-26 16:08:50 +0900

    update

--- 2021.08.29 raspberry_pi, opencv, tensorflow/2. 라즈베리파이 색상 식별(tensorflow)/my_find_color_tflite.py
+++ 2021.08.29 raspberry_pi, opencv, tensorflow/2. 라즈베리파이 색상 식별(tensorflow)/my_find_color_tflite.py
@@ -3,24 +3,31 @@ import numpy as np
 import tflite_runtime.interpreter as tflite
 from gpiozero import LED
 
+# LED 설정
 red_led = LED(14)
 green_led = LED(15)
 blue_led = LED(18)
 yellow_led = LED(17)
 
+# 라벨 생성
 label = ['red', 'green', 'blue', 'yellow', 'none']
+
+# 모델 로드
 interpreter = tflite.Interpreter(model_path='model/model_unquant.tflite')
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
+# print(input_details)
 
+# 카메라 캡쳐 시작
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
 
     if ret:
+        # input_details 조건에 맞게 형식 변경
         frame = cv2.resize(frame, dsize=(224, 224))
         dst = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         cv2.imshow('video', frame)
@@ -32,8 +39,8 @@ while True:
         output_data = interpreter.get_tensor(output_details[0]['index'])
 
         result = label[np.argmax(output_data[0])]
-        #print(result, *output_data)
-        
+        # print(result, *output_data)
+
         if result == 'red':
             red_led.on()
             blue_led.off()
@@ -59,9 +66,9 @@ while True:
             blue_led.off()
             green_led.off()
             yellow_led.off()
-            
+
         print(result)
-        
+
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
